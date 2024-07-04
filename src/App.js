17c22849aa38d797ae783f2ec67a47b7445e736e
Terer <terermike3@gmail.com>
2024-07-05 01:16:54 +0300

    Added css classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
 export default function HomePage() {
-  return <p>Welcome to the Home Page</p>;
+  return (
+    <>
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        Go to <Link to={"/products"}> the list of products</Link>
+      </p>
+    </>
+  );
 }
