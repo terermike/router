ba7305df75fdb4f740ebab9f361f81c2554d03d7
Terer <terermike3@gmail.com>
2024-07-05 14:47:54 +0300

    Modified files to use Error.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetails from "./pages/ProductDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:id", element: <ProductDetails /> },
     ],
   },
 ]);
