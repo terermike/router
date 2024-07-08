7449020e54b1bfbb6daa1d617e015ad75bbbdae2
Terer <terermike3@gmail.com>
2024-07-08 23:45:16 +0300

    Modified the file to utilize dynamic routing

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+const PRODUCTS = [
+  { id: "p1", title: "product-1" },
+  { id: "p2", title: "product-2" },
+  { id: "p3", title: "product-3" },
+];
+
 export default function ProductsPage() {
-  return <p>Products Page</p>;
+  return (
+    <>
+      <p>Products Page</p>
+      <ul>
+        {PRODUCTS.map((item) => (
+          <li key={item.id}>
+            <Link to={`/products/${item.id}`}> {item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
